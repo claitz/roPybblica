1462922db8b18d5af0b89410f56f54ad860ab467
claitz <stefanomus@fastmail.com>
2020-11-15 16:02:27 +0000

    Changed scope

--- loadNews.py
+++ loadNews.py
@@ -1,51 +1,14 @@
 import newspaper
+from newspaper import news_pool
 
 repubblica = newspaper.build(url='https://www.repubblica.it/', language='it', fetch_images=False, memoize_articles=False)
-wired_i = newspaper.build(url='https://www.wired.it/', verbose=True, language='it', fetch_images=False, memoize_articles=False)
 corriere = newspaper.build(url='https://www.corriere.it/', language='it', fetch_images=False, memoize_articles=False)
+tpi = newspaper.build(url='https://www.tpi.it/', language='it', fetch_images=False, memoize_articles=False)
 
-papers = [repubblica, wired_i]
-# news_pool.set(papers, threads_per_source=2)
-# news_pool.join()
+papers = [repubblica, corriere, tpi]
+news_pool.set(papers, threads_per_source=2) # (3*2) = 6 threads total
+news_pool.join()
 
-articleNum = 5
-
-for article in range(int(articleNum)):
-    current_article = wired_i.articles[article+21]
-    current_article.download()
-    current_article.parse()
-    print('Titolo: ' + current_article.title)
-    print('Aggiungo il testo a news.txt')
-    with open('raw_text/news.txt', 'a') as newsFile:
-        newsFile.write(current_article.text)
-        # newsFile.write('\n')
-        newsFile.close()
-    print('Fatto.')
-
-for article in range(int(articleNum)):
-    current_article = repubblica.articles[article]
-    current_article.download()
-    current_article.parse()
-    print('Titolo: ' + current_article.title)
-    print('Aggiungo il testo a news.txt')
-    with open('raw_text/news.txt', 'a') as newsFile:
-        newsFile.write(current_article.text)
-        # newsFile.write('\n')
-        newsFile.close()
-    print('Fatto.')
-
-for article in range(int(articleNum)):
-    current_article = corriere.articles[article]
-    current_article.download()
-    current_article.parse()
-    print('Titolo: ' + current_article.title)
-    print('Aggiungo il testo a news.txt')
-    with open('raw_text/news.txt', 'a') as newsFile:
-        newsFile.write(current_article.text)
-        # newsFile.write('\n')
-        newsFile.close()
-    print('Fatto.')
-
-
-# for article in wired_i.articles:
-#     print(article.title)
+print(repubblica.articles[10].title)
+print(corriere.articles[10].title)
+print(tpi.articles[10].title)
--- pyMeme.py
+++ pyMeme.py
@@ -1,23 +1,26 @@
 from PIL import Image, ImageDraw, ImageFont
 import markovify
 import textwrap
+import random
 
 #### SETUP ####
 
 # Assets locations
 fontName = 'assets/Roboto.ttf'
-rawText = 'raw_text/news.txt'
-bgImage = 'assets/blank_error_single.png'
+rawText = 'raw_text/
+.txt'
+# bgImage = 'assets/blank_error_single.png'
 
 # Load the raw text
 with open(rawText) as f:
     text = f.read()
 
 # Build the mode
-text_model = markovify.Text(text)
+# text_model = markovify.Text(text)
+text_model = markovify.NewlineText(text)
 
 # set font and size
-font = ImageFont.truetype(fontName, size=35)
+font = ImageFont.truetype(fontName, size=50)
 fontM = ImageFont.truetype(fontName, size=30)
 fontS = ImageFont.truetype(fontName, size=25)
 fontXS = ImageFont.truetype(fontName, size=15)
@@ -34,6 +37,9 @@ print('Ok, I\'m going to generate ' + str(imgNum) + ' images..')
 
 for nn in range(int(imgNum)):
 
+    rndImg = random.randint(1,5)
+    bgImage = 'assets/' + str(rndImg) + '.png'
+
     # create Image object with the input image
     image = Image.open(bgImage)
     
@@ -44,7 +50,7 @@ for nn in range(int(imgNum)):
     message = text_model.make_short_sentence(300)
 
     # wrap the text at 50 chars
-    wrapper = textwrap.TextWrapper(width=50) 
+    wrapper = textwrap.TextWrapper(width=30) 
     word_list = wrapper.wrap(text=message) 
     message_wrap = ''
     for ii in word_list[:-1]:
@@ -52,24 +58,24 @@ for nn in range(int(imgNum)):
     message_wrap += word_list[-1]
     
     ### SET THE TEXTS ###
-    # window title
-    (x, y) = (55, 15)
-    title = "Alert"
-    color = 'rgb(255, 255, 255)'
-    # draw the title
-    draw.text((x, y), title, fill=color, font=fontM)
+    # # window title
+    # (x, y) = (55, 15)
+    # title = "Alert"
+    # color = 'rgb(255, 255, 255)'
+    # # draw the title
+    # draw.text((x, y), title, fill=color, font=fontM)
 
     # main message
-    (x, y) = (130, 80)
+    (x, y) = (80, 80)
     color = 'rgb(0, 0, 0)'
-    draw.multiline_text((x,y), message_wrap, fill=color, font=fontXS, anchor=None, spacing=3, align='left', direction=None, features=None, language=None, stroke_width=0, stroke_fill=None, embedded_color=False)
+    draw.multiline_text((x,y), message_wrap, fill=color, font=font, anchor=None, spacing=3, align='left', direction=None, features=None, language=None, stroke_width=0, stroke_fill=None, embedded_color=False)
 
-    # button message
-    (x, y) = (190, 205)
-    CTA = 'OK'
-    color = 'rgb(0, 0, 0)'
-    #draw.text((x, y), CTA, fill=color, align='center', font=fontS)
-    draw.multiline_text((x,y), CTA, fill=color, font=fontS, anchor=None, spacing=3, align='center', direction=None, features=None, language=None, stroke_width=0, stroke_fill=None, embedded_color=False)
+    # # button message
+    # (x, y) = (190, 205)
+    # CTA = 'OK'
+    # color = 'rgb(0, 0, 0)'
+    # #draw.text((x, y), CTA, fill=color, align='center', font=fontS)
+    # draw.multiline_text((x,y), CTA, fill=color, font=fontS, anchor=None, spacing=3, align='center', direction=None, features=None, language=None, stroke_width=0, stroke_fill=None, embedded_color=False)
 
     # save the edited image
     if fileName == '':
